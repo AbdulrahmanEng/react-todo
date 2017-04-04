b23aa8bedd0018277d375a19863092d87974c616
Abdulrahman I <bydeinon@gmail.com>
2017-04-04 14:41:15 +0000

    Add Form and mock todo items

--- src/App.js
+++ src/App.js
@@ -10,9 +10,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+				<div className="Todo-App">
+					<form>
+						<input type="text"/>
+					</form>
+					<div className="Todo-List">
+						<ul>
+							<li><input type="checkbox"/>Learn JavaScript</li>
+							<li><input type="checkbox"/>Build App</li>
+							<li><input type="checkbox"/>Learn Greek</li>
+						</ul>
+					</div>
+				</div>
       </div>
     );
   }
